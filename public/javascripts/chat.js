d02f1bb50a41ab0baa6263fe21fbb4e9e004c82e
Michael Darmousseh <michael@milyoni.com>
2014-01-22 17:13:44 -0800

    link it

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,3 +1,11 @@
+jQuery.fn.autolink = function (target) {
+  if (target == null) target = '_parent';
+  return this.each( function(){
+    var re = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+    $(this).html( $(this).html().replace(re, '<a href="$1" target="'+ target +'">$1</a> ') );
+  });
+}
+
 $(document).ready(function() {
   var messages = [];
   var socket = io.connect('/');
@@ -33,6 +41,7 @@ $(document).ready(function() {
         html += messages[i].message + '<br />';
       }
       content.innerHTML = html;
+      $(content).autolink();
       content.scrollTop = content.scrollHeight;
     } else {
       console.log("There is a problem:", data);
